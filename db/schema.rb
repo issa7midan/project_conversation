e56981b1f2659b2de70fa585cb144bea9f416c1e
Issa Hmidan <issa@zenhr.com>
2025-04-04 16:51:36 +0300

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_04_04_114215) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "project_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "project_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_comments_on_project_id"
@@ -31,9 +34,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_04_114215) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.integer "company_id", null: false
+    t.bigint "company_id", null: false
     t.integer "status", default: 0
-    t.integer "owner_id", null: false
+    t.bigint "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_projects_on_company_id"
@@ -43,7 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_04_114215) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "company_id", null: false
+    t.bigint "company_id", null: false
     t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
